dd71d2a186ae884e82c2c5e66af575a9be28baa0
Torkel <torkel@voysys.se>
2019-08-24 23:23:02 +0200

    initial commit, include of argus works

---
+++ README.md
@@ -0,0 +1,5 @@
+Download "L4T Multimedia API" from nvidia.
+
+Build command:
+clang camera_test.cpp -Iinclude && ./a.out
+
---
+++ camera_test.cpp
@@ -0,0 +1,13 @@
+#include "Argus/Argus.h"
+#include "EGLStream/NV/ImageNativeBuffer.h"
+#include "nvbuf_utils.h"
+#include "NvBuffer.h"
+#include "EGLStream/EGLStream.h"
+//#include "NvVideoConverter.h"
+
+#include <cstdio>
+
+int main()
+{
+    printf("Hello world\n");
+}
